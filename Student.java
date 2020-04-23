13f99d486f92352ae1ccfe69b440156d03ed671b
sal119 <hatl.sal19@gmail.com>
2020-04-23 13:22:44 +0200

    Create Student class

---
+++ Student.java
@@ -0,0 +1,23 @@
+Public class Studnet
+{
+	private String name;
+	private int alt;
+
+	public void  setName(String name)
+	{
+		this.name=name;
+	}
+	public void  setAlte(int alt)
+	{
+		this.alt=alt;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getAlt()
+	{
+		return alt;
+	}
+
+}
